f408095ca0ae912a40d6fca67bc710b5266614d7
bskmrkvc <markovic.bosko2000@gmail.com>
2023-11-09 15:50:20 +0100

    Updated CSS

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function toggleDescription(sectionNumber) {
-    const toggle = document.querySelector(`.section:nth-of-type(${sectionNumber}) .toggle`);
-    const description = document.querySelector(`.section:nth-of-type(${sectionNumber}) .description`);
-  
-    toggle.textContent = toggle.textContent === '+' ? '-' : '+';
-    description.classList.toggle('open');
-  }
-  
+  const toggles = document.querySelectorAll('.toggle');
+  const descriptions = document.querySelectorAll('.description');
+
+  toggles.forEach((toggle, index) => {
+    if (index + 1 === sectionNumber) {
+      toggle.textContent = toggle.textContent === '+' ? '-' : '+';
+      descriptions[index].classList.toggle('open');
+    } else {
+      toggle.textContent = '+';
+      descriptions[index].classList.remove('open');
+    }
+  });
+}
